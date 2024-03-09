fd45e44cdbccc0fd4cfe09515ad4197f5f4ae9e5
Suyash Shringarpure <suyashss+github@gmail.com>
2024-03-09 09:57:27 -0800

    Handle keyerrors

--- utils.py
+++ utils.py
@@ -16,11 +16,15 @@ def check_equal_yaml_files(fname1,fname2):
 def check_equal_yaml_strings(str1,str2):
     try:
         graph1 = demes.load(str1)
+    except KeyError as k:
+        print(k)
     except Exception as e:
         print(f'Error loading {str1}')
         return False
     try:
         graph2 = demes.load(str2)
+    except KeyError as k:
+        print(k)
     except Exception as e:
         print(f'Error loading {str2}')
         return False
